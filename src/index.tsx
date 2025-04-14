9df6133ebe57d0b0e6bba4af383fccfbb914d6ed
BBIL-Team <deepshika5686@bharatbiotech.com>
2025-04-14 15:47:39 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
-import awsconfig from './aws-exports'; // Ensure this file exists
+// import awsconfig from './aws-exports'; // Ensure this file exists
 import App from './App';
 import './index.css'; // Optional, if you have global styles
 
